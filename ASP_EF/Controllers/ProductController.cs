03f52626c19a63c5552469f796733da551c5a7e3
Burhan <burhanorucov@gmail.com>
2026-01-29 18:45:41 +0400

    Refactor ProductController to remove repository methods
    Removed repository dependency and actions from ProductController.

--- ASP_EF/Controllers/ProductController.cs
+++ ASP_EF/Controllers/ProductController.cs
@@ -1,40 +1,12 @@
-﻿using ASP_EF.Models;
 using Microsoft.AspNetCore.Mvc;
 
-public class ProductController : Controller
+namespace ASP_EF.Controllers
 {
-    private readonly IProductRepository _repo;
-
-    public ProductController(IProductRepository repo)
+    public class ProductController : Controller
     {
-        _repo = repo;
-    }
-
-    public async Task<IActionResult> Index()
-    {
-        return View(await _repo.GetAllAsync());
-    }
-
-    public async Task<IActionResult> Details(int id)
-    {
-        return View(await _repo.GetByIdAsync(id));
-    }
-
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Create(Product product)
-    {
-        await _repo.AddAsync(product);
-        return RedirectToAction(nameof(Index));
-    }
-
-    public async Task<IActionResult> Delete(int id)
-    {
-        await _repo.DeleteAsync(id);
-        return RedirectToAction(nameof(Index));
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
